460cbbe37e7e9e443d8225267238a6cf48593e58
lei.yao <yoing945@outlook.com>
2019-05-02 21:09:01 +0800

    update Vector3.h

--- Project/Vector3/Vector3/Vector3.h
+++ Project/Vector3/Vector3/Vector3.h
@@ -10,6 +10,42 @@ public:
 	Vector3(const Vector3& a) :x(a.x), y(a.y), z(a.z) {}
 	Vector3(float nx, float ny, float nz) :x(nx), y(ny), z(nz) {}
 
-	//override Assignment operator
 	Vector3& operator = (const Vector3& a);
+
+	bool operator == (const Vector3& a);
+
+	bool operator != (const Vector3& a);
+
+	//置为零向量
+	void zero();
+
+	//重载一元 "-"(负号运算符)
+	Vector3 operator - ();
+
+	//重载二元+运算符
+	Vector3 operator + (const Vector3 &a);
+
+	//重载二元-运算符
+	Vector3 operator - (const Vector3 &a);
+
+	//与标量乘法
+	Vector3 operator * (const float a);
+
+	//与标量除法
+	Vector3 operator / (const float a);
+
+	//自增
+	Vector3 &operator += (const Vector3 &a);
+
+	//自减
+	Vector3 &operator -= (const Vector3 &a);
+
+	//自乘
+	Vector3 &operator *= (const float a);
+
+	//自除
+	Vector3 &operator /= (const float a);
+
+	//向量标准化
+	void normallize();
 };
